ccc8ea2d5578e0a4730ed2f996b9f65ef6adfa24
Sean Takahashi <seanyuto@gmail.com>
2025-04-16 16:55:54 -0400

    added new navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'; 
 import Home from './pages/Home';
 import CV from './pages/CV';
 
 function App() {
   return (
     <Router>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/CV" element={<CV />} />
