66ef5bb111967822e544176bdf315fc6c1e2d03c
Sean Takahashi <seanyuto@gmail.com>
2025-04-16 17:37:44 -0400

    added className to link with CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
         <Link to="/" className="navbar-logo">Sean Takahashi</Link>  {/* Your name/logo links to home */}
         <ul className="navbar-links">
           <li><Link to="/" className="navbar-link">Home</Link></li>
-          <li><a href="/resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
+          <li><a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="navbar-link">Resume</a></li>
           <li><Link to="/projects" className="navbar-link">Projects</Link></li>
         </ul>
       </div>
