b5b2fb208255d66a0819353ed2ce50595d60b27e
Sean Takahashi <seanyuto@gmail.com>
2025-04-16 16:51:44 -0400

    changed links to linked logos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import './Home.css';
 
 const Home = () => {
@@ -6,15 +7,19 @@ const Home = () => {
     <div className="home-container">
       <h1>Sean Takahashi</h1>
       <p>
-        My name is Sean Takahashi and I'm currently a junior at <a href="https://stuy.enschool.org" target="_blank" rel="noopener noreferrer">Stuyvesant High School</a>. My interests lie in using ethical data collection and using Machine Learning to optimize user services.  
+        My name is Sean Takahashi and I'm currently a junior at <a href="https://stuy.enschool.org" target="_blank" rel="noopener noreferrer">Stuyvesant High School</a>. 
+        My interests lie in using ethical data collection and using machine learning to optimize user services.  
       </p>
       <p>
         Check out my <a href="/CV">resume</a>, view my projects, or connect with me below!
       </p>
       <div className="social-links">
-        <a href="https://github.com/syt-cs" target="_blank" rel="noopener noreferrer">GitHub</a> | 
-        <a href="https://linkedin.com/in/____" target="_blank" rel="noopener noreferrer"> LinkedIn</a> |
-        <a href="seanyuto@gmail.com"> Email</a>
+        <a href="https://github.com/syt-cs" target="_blank" rel="noopener noreferrer">
+        <FaGithub size={40} color="#3498db" />
+        </a>
+        <a href="https://linkedin.com/in/seantakahashi" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={40} color="#0077b5" />
+        </a>
       </div>
     </div>
   );
