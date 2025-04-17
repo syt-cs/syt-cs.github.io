25c55264f7b797c80cc10a3b8d9c4ab5beebdfe7
Sean Takahashi <seanyuto@gmail.com>
2025-04-17 01:22:50 -0400

    changed home to about page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">sean takahashi.</Link>  {/* Your name/logo links to home */}
         <ul className="navbar-links">
-          <li><Link to="/" className="navbar-link">home</Link></li>
+          <li><Link to="/about" className="navbar-link">about</Link></li>
           <li><a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="navbar-link">resume</a></li>
           <li><Link to="/projects" className="navbar-link">projects</Link></li>
         </ul>
