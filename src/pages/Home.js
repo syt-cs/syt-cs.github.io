0300264a1911b2e2965103b7b498d6adf3c99bc4
Sean Takahashi <seanyuto@gmail.com>
2025-04-17 01:43:42 -0400

    changed button to hover over logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
-  const texts = [
+  const phrases = [
     "is a programmer",
     "is a swimmer",
     "wants to travel to Europe",
@@ -12,44 +12,37 @@ const Home = () => {
     "loves the outdoors"
   ];
 
-  const [textIndex, setTextIndex] = useState(0);
+  const [displayText, setDisplayText] = useState("Takahashi");
   const [isHovering, setIsHovering] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!isHovering) {
-      const interval = setInterval(() => {
-        setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
-      }, 1500); // Change every 1.5 seconds
-
-      return () => clearInterval(interval);
-    }
-  }, [isHovering]);
-
   const handleMouseEnter = () => {
     setIsHovering(true);
-    const randomIndex = Math.floor(Math.random() * texts.length);
-    setTextIndex(randomIndex);
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    setDisplayText(phrases[randomIndex]);
   };
 
   const handleMouseLeave = () => {
     setIsHovering(false);
+    setDisplayText("Takahashi");
   };
 
-  const handleNavigateToAbout = () => {
+  const handleClick = () => {
     navigate('/about');
   };
 
   return (
     <div className="home-container">
-      <h1 className="home-title">Sean</h1>
-      <div
-        className="hover-takahashi"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        onClick={handleNavigateToAbout}
-      >
-        Takahashi
+      <div className="text-block">
+        <div className="line-sean">Sean</div>
+        <div
+          className="line-takahashi"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onClick={handleClick}
+        >
+          {displayText}
+        </div>
       </div>
     </div>
   );
