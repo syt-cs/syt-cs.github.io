3554ad1b3ce67d140773beebbf15480623dd0ed0
Sean Takahashi <seanyuto@gmail.com>
2025-04-17 01:23:03 -0400

    added links for about page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'; 
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import About from './pages/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
